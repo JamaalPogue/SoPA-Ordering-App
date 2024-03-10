5ab1fd049002eb6f75d9dbb5fab7e6aa811dcf09
Jamaal Pogue <jpogue1@tulane.edu>
2024-03-09 17:51:30 -0800

    Add table creation and alter statements

--- databaseAdmin.py
+++ databaseAdmin.py
@@ -10,12 +10,112 @@ def connectToDatabase(host_name, user_name, user_password, db_name):
             passwd=user_password,
             database=db_name
         )
-        print("Connection to MySQL DB successful")
+        print("Connection to MySQL DB successful.")
     except Error as e:
         print(f"The error '{e}' occurred")
 
     return connection
 
+# This is the block of table creation statements
+def createUserRoleTable(connection, cursor):
+    cursor.execute("CREATE TABLE IF NOT EXISTS UserRole (UserRoleID int NOT NULL,RoleDescription varchar(50) UNIQUE,PRIMARY KEY (UserRoleID))")
+    connection.commit()
+    print("UserRole table created.")
+
+def createUsersTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  Users (UserID int NOT NULL, FirstName varchar(100) NOT NULL, LastName varchar(100) NOT NULL, UserRoleID int, UserEmail varchar(100), PreferredPaymentMethod varchar(100), isDeleted BOOLEAN DEFAULT FALSE, PRIMARY KEY (UserID))")
+    connection.commit()
+    print("Users table created.")
+
+def createDistributionCenterTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  DistributionCenter (SiteID int NOT NULL, DODAddressCode varchar(10), FacilityNo int NOT NULL, FacilityNoLong int, SiteName varchar(255), SitePhone varchar(100), ShippingAddress varchar(255), ShippingAddress2 varchar(255), ShippingAddress3 varchar(255), ShippingAddress4 varchar(255), ShippingCity varchar(255), ShippingState varchar(100), ShippingZip varchar(100), PRIMARY KEY (SiteID))")
+    connection.commit()
+    print("DistributionCenter table created.")
+
+def createOrdersTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS Orders (OrderID int NOT NULL, UserID int, SiteID int, OrderDetails varchar(255), TotalCost decimal(10,2), OrderStatus varchar(255) CHECK (OrderStatus IN ('Pending', 'Completed', 'Canceled')), PRIMARY KEY (OrderID))")
+    connection.commit()
+    print("Orders table created.")
+
+def createAuthenticationTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  Authentication (AuthenticationID int NOT NULL, UserID int, HashedPassword varchar(100), LastPasswordChangeDate date, PasswordChangeRequired boolean DEFAULT FALSE, PRIMARY KEY (AuthenticationID))")
+    connection.commit()
+    print("Authentication table created.")
+
+def createOrderDetailTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  OrderDetail (OrderDetailID int NOT NULL, OrderID int, ProductID int, Quantity int, Customized boolean, CustomizationID int, PRIMARY KEY (OrderDetailID))")
+    connection.commit()
+    print("OrderDetail table created.")
+
+def createCustomizationTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  Customization (CustomizationID int NOT NULL, ItemDescription varchar(255), PRIMARY KEY (CustomizationID))")
+    connection.commit()
+    print("Customization table created.")
+
+def createProductsTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  Products (ProductID int NOT NULL, ProductName varchar(255) NOT NULL UNIQUE, ProductColor varchar(100), ItemDescription varchar(255), Price decimal(5,2)  CHECK (Price >= 0), PRIMARY KEY (ProductID))")
+    connection.commit()
+    print("Products table created.")
+
+def createInventoryTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  Inventory (InventoryID int NOT NULL, ProductID int, CurrentStockLevel int NOT NULL CHECK (CurrentStockLevel >= 0), PRIMARY KEY (InventoryID))")
+    connection.commit()
+    print("Inventory table created.")
+
+def createAuditLogTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  AuditLog (LogID int NOT NULL, UserID int, ActivityType varchar(255), ActivityTimestamp date, AffectedRecordID int, ItemDescription varchar(255), PRIMARY KEY (LogID))")
+    connection.commit()
+    print("AuditLog table created.")
+
+def createWarehouseNotificationTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  WarehouseNotification (NotificationID int NOT NULL, OrderID int, SiteID int, NotificationTimestamp datetime, PRIMARY KEY (NotificationID))")
+    connection.commit()
+    print("WarehouseNotification table created.")
+
+def createPaymentNotificationTable(connection, cursor):
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS  PaymentNotification (PaymentID int NOT NULL, OrderID int, Timestamp datetime, PRIMARY KEY (PaymentID))")
+    connection.commit()
+    print("PaymentNotification table created.")
+
+def runAlterStatements(connection, cursor):
+    cursor.execute("ALTER TABLE Users ADD FOREIGN KEY (UserRoleID) REFERENCES UserRole (UserRoleID);")
+    cursor.execute("ALTER TABLE Authentication ADD FOREIGN KEY (UserID) REFERENCES Users (UserID);")
+    cursor.execute("ALTER TABLE Orders ADD FOREIGN KEY (UserID) REFERENCES Users (UserID);")
+    cursor.execute("ALTER TABLE Orders ADD FOREIGN KEY (SiteID) REFERENCES DistributionCenter (SiteID);")
+    cursor.execute("ALTER TABLE OrderDetail ADD FOREIGN KEY (OrderID) REFERENCES Orders (OrderID);")
+    cursor.execute("ALTER TABLE OrderDetail ADD FOREIGN KEY (ProductID) REFERENCES Products (ProductID);")
+    cursor.execute("ALTER TABLE OrderDetail ADD FOREIGN KEY (CustomizationID) REFERENCES Customization (CustomizationID);")
+    cursor.execute("ALTER TABLE Inventory ADD FOREIGN KEY (ProductID) REFERENCES Products (ProductID);")
+    cursor.execute("ALTER TABLE AuditLog ADD FOREIGN KEY (UserID) REFERENCES Users (UserID);")
+    cursor.execute("ALTER TABLE WarehouseNotification ADD FOREIGN KEY (OrderID) REFERENCES Orders (OrderID);")
+    cursor.execute("ALTER TABLE WarehouseNotification ADD FOREIGN KEY (SiteID) REFERENCES DistributionCenter (SiteID);")
+    cursor.execute("ALTER TABLE PaymentNotification ADD FOREIGN KEY (OrderID) REFERENCES Orders (OrderID);")
+    connection.commit()
+    print("Alter statements successful.")
+
+def createProcedureSoftDeleteUser(connection,cursor):
+    softDeleteUser = """
+    CREATE PROCEDURE IF NOT EXISTS softDeleteUser (IN deletedUserID INT)
+    BEGIN
+        UPDATE Users
+        SET isDeleted = TRUE
+        WHERE userID = deletedUserID;
+    END
+    """
+    cursor.execute(softDeleteUser)
+    connection.commit()
+    print("softDeleteUser procedure created.")
 
 # Read all from user table
 def readAllUsers(cursor):
@@ -24,7 +124,6 @@ def readAllUsers(cursor):
     print("This is a test to pull all data from users table.")
     print(cursor.fetchall())
     print("Test successful. Congrats, this works.")
-
 
 if __name__ == "__main__":
 
@@ -39,6 +138,24 @@ if __name__ == "__main__":
 
     # Initialize database cursor
     cursor = connection.cursor()
+
+    # Create all tables and alter statements
+    createUserRoleTable(connection, cursor)
+    createUsersTable(connection, cursor)
+    createDistributionCenterTable(connection, cursor)
+    createOrdersTable(connection, cursor)
+    createAuthenticationTable(connection, cursor)
+    createOrderDetailTable(connection, cursor)
+    createCustomizationTable(connection, cursor)
+    createProductsTable(connection, cursor)
+    createInventoryTable(connection, cursor)
+    createAuditLogTable(connection, cursor)
+    createWarehouseNotificationTable(connection, cursor)
+    createPaymentNotificationTable(connection, cursor)
+    runAlterStatements(connection, cursor)
+
+    # Create procedures
+    createProcedureSoftDeleteUser(connection, cursor)
 
     # Read user table
     readAllUsers(cursor)
